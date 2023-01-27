be1cba2db95cd1d00e85c1624f218693e68f1a62
Jakob Giese <gooserjg@hotmail.com>
2023-01-27 17:03:37 -0500

    modularize location info

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const term = require('terminal-kit').terminal;
-const setTimeout = require('timers/promises').setTimeout;
+const { setTimeout } = require('timers/promises');
+const LocationInfos = require('./location-infos');
 const hat = '^R^^';
 const hole = '^cO';
 const fieldCharacter = '^Y░';
@@ -9,53 +10,6 @@ const LEFT = 'LEFT';
 const RIGHT = 'RIGHT';
 const UP = 'UP';
 const DOWN = 'DOWN';
-
-class LocationInfo {
-    constructor({LEFT = [], RIGHT = [], UP = [], DOWN = []}) {
-        this.LEFT = LEFT;
-        this.RIGHT = RIGHT;
-        this.UP = UP;
-        this.DOWN = DOWN;
-        this._bannedDirs = [];
-    }
-
-    hasBannedDir(direction) {
-        return this._bannedDirs.includes(direction);
-    }
-
-    // only ever add one of a banned direction
-    addOneBannedDir(direction) {
-        if (!this.hasBannedDir(direction)){
-            this._bannedDirs.push(direction);
-        }
-    }
-
-    updateLocationDirectionInfo({LEFT = [], RIGHT = [], UP = [], DOWN = []}){
-        this.LEFT = LEFT;
-        this.RIGHT = RIGHT;
-        this.UP = UP;
-        this.DOWN = DOWN;
-    }
-}
-
-class LocationInfos {
-    constructor(locationInfos = {}) {
-        this._locationInfos = locationInfos;
-    }
-
-    setLocationInfo([xLocation, yLocation], data) {
-        const locationKey = xLocation + '-' + yLocation;
-        if (!this._locationInfos[locationKey]){
-            this._locationInfos[locationKey] = new LocationInfo(data);
-        } else {
-            this._locationInfos[locationKey].updateLocationDirectionInfo(data);
-        }
-    }
-
-    getLocationInfo(xLocation, yLocation) {
-        return this._locationInfos[xLocation + '-' + yLocation];
-    }
-}
 
 class Field {
     constructor(field = [], opts) {
@@ -313,7 +267,7 @@ class Field {
                 const prioritizedDirs = hatDirections.sort((aDirection, bDirection) => {
                     const [, nextXLocA, nextYLocA] = locationInfo[aDirection];
                     const [, nextXLocB, nextYLocB] = locationInfo[bDirection];
-                    if (locationInfos.getLocationInfo(nextXLocA, nextYLocA) && !locationInfos.getLocationInfo(nextXLocB,nextYLocB)) {
+                    if (locationInfos.getLocationInfo(nextXLocA, nextYLocA) && !locationInfos.getLocationInfo(nextXLocB, nextYLocB)) {
                         return 1;
                     } else if (locationInfos.getLocationInfo(nextXLocA, nextYLocA) && locationInfos.getLocationInfo(nextXLocB, nextYLocB)) {
                         return -1;

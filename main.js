902aa9b808ab9424892a50fbb16b2b3617efc6b9
Jakob Giese <gooserjg@hotmail.com>
2023-01-27 17:03:37 -0500

    clean up the canSolve logic by creating some classes

--- main.js
+++ main.js
@@ -10,10 +10,56 @@ const RIGHT = 'RIGHT';
 const UP = 'UP';
 const DOWN = 'DOWN';
 
+class LocationInfo {
+    constructor({LEFT = [], RIGHT = [], UP = [], DOWN = []}) {
+        this.LEFT = LEFT;
+        this.RIGHT = RIGHT;
+        this.UP = UP;
+        this.DOWN = DOWN;
+        this._bannedDirs = [];
+    }
+
+    hasBannedDir(direction) {
+        return this._bannedDirs.includes(direction);
+    }
+
+    // only ever add one of a banned direction
+    addOneBannedDir(direction) {
+        if (!this.hasBannedDir(direction)){
+            this._bannedDirs.push(direction);
+        }
+    }
+
+    updateLocationDirectionInfo({LEFT = [], RIGHT = [], UP = [], DOWN = []}){
+        this.LEFT = LEFT;
+        this.RIGHT = RIGHT;
+        this.UP = UP;
+        this.DOWN = DOWN;
+    }
+}
+
+class LocationInfos {
+    constructor(locationInfos = {}) {
+        this._locationInfos = locationInfos;
+    }
+
+    setLocationInfo([xLocation, yLocation], data) {
+        const locationKey = xLocation + '-' + yLocation;
+        if (!this._locationInfos[locationKey]){
+            this._locationInfos[locationKey] = new LocationInfo(data);
+        } else {
+            this._locationInfos[locationKey].updateLocationDirectionInfo(data);
+        }
+    }
+
+    getLocationInfo(xLocation, yLocation) {
+        return this._locationInfos[xLocation + '-' + yLocation];
+    }
+}
 
 class Field {
     constructor(field = [], opts) {
-        const  {hardMode = false, randomStart = false, simulate = false} = opts || {};
+        const { hardMode = false, randomStart = false, simulate = false } = opts || {};
         this._field = field;
         this.hardMode = hardMode;
         if (randomStart) {
@@ -36,27 +82,22 @@ class Field {
         return char === hat;
     }
 
-    static isHoleChar(char){
+    static isHoleChar(char) {
         return char === hole;
     }
 
     toString() {
-        return this._field.join('\n').split(',').join(''); 
+        return this._field.join('\n').split(',').join('');
     }
 
     print() {
-        // probably remove . . . or figure out how to not reprint?
-        // let newlines = '';
-        // for (let i = 0; i < 100; i+=1 ){
-        //     newlines += '\n';
-        // }
         term.reset();
         term(this.toString());
     }
 
     static getEmptyLoc(field, height, width) {
         const isHatOrHoleOrStartOrPath = (location) => {
-            const [x,y] = location;
+            const [x, y] = location;
             const value = field[y][x];
             return value === hole || value === hat || value === pathCharacter || value === activePathCharacter || (!x && !y);
         }
@@ -73,15 +114,15 @@ class Field {
     static generateField(height = 10, width = 5, percentageHoles = 20) {
         const percentHolesFloat = percentageHoles / 100;
         let numHoles = Math.floor(height * width * percentHolesFloat);
-        
+
         // a field is an array of many row arrays
         const field = [];
         // draw empty field
         // height
-        for (let y = 0; y < height; y+=1){
+        for (let y = 0; y < height; y += 1) {
             // width
             let row = [];
-            for (let x = 0; x < width; x+=1){   
+            for (let x = 0; x < width; x += 1) {
                 row.push(fieldCharacter);
             }
 
@@ -92,8 +133,8 @@ class Field {
         const [hatX, hatY] = Field.getEmptyLoc(field, height, width);
         field[hatY][hatX] = hat;
         // locate holes
-        
-        for (numHoles; numHoles > 0; numHoles-=1) {
+
+        for (numHoles; numHoles > 0; numHoles -= 1) {
             const [holeX, holeY] = Field.getEmptyLoc(field, height, width);
             field[holeY][holeX] = hole;
         }
@@ -105,37 +146,37 @@ class Field {
         this.gameOverText = '\n' + gameOverText;
         this.isGameOver = true;
     }
-    
-    getCurrentChar(){
-        if (!this._field[this._yPlayerLocation]){
+
+    getCurrentChar() {
+        if (!this._field[this._yPlayerLocation]) {
             return null;
         }
-        if (!this._field[this._yPlayerLocation][this._xPlayerLocation]){
+        if (!this._field[this._yPlayerLocation][this._xPlayerLocation]) {
             return null;
         }
 
         return this._field[this._yPlayerLocation][this._xPlayerLocation];
     }
 
-    checkAtHat(){
-        if (Field.isHatChar(this.getCurrentChar())){
+    checkAtHat() {
+        if (Field.isHatChar(this.getCurrentChar())) {
             this.endGame('You found the hat! Congratulations!')
         }
     }
 
-    checkInHole(){
-        if (Field.isHoleChar(this.getCurrentChar())){
+    checkInHole() {
+        if (Field.isHoleChar(this.getCurrentChar())) {
             this.endGame('Sorry, you fell down a hole. Please try again.')
         }
     }
 
-    checkOutOfBounds(){
-        if (!this.getCurrentChar()){
+    checkOutOfBounds() {
+        if (!this.getCurrentChar()) {
             this.endGame('Sorry, you\'ve fallen out of bounds. Please try again.')
         }
     }
 
-    getHeight(){
+    getHeight() {
         return this._field.length;
     }
 
@@ -148,48 +189,48 @@ class Field {
         const width = this.getWidth();
         const getNumHolesPercent = () => {
             let numHoles = 0;
-            this._field.forEach((row)=>{
+            this._field.forEach((row) => {
                 row.forEach((char) => {
                     if (char === hole) {
-                        numHoles+=1;
+                        numHoles += 1;
                     }
                 })
             });
+            // 5/25 = x/100 numholes * 100 / total
             return Math.floor(numHoles * 100 / (height * width));
         }
-        // 5/25 = x/100 numholes * 100 / total
         this._field = Field.generateField(height, width, getNumHolesPercent());
     }
 
     addRandomNumHoles() {
-        const possibleNumHoles = [0,1,2];
+        const possibleNumHoles = [0, 1, 2];
         let numHolesToAdd = possibleNumHoles[Math.floor(Math.random() * possibleNumHoles.length)];
-        for (numHolesToAdd; numHolesToAdd > 0; numHolesToAdd-=1) {
+        for (numHolesToAdd; numHolesToAdd > 0; numHolesToAdd -= 1) {
             const [holeX, holeY] = Field.getEmptyLoc(this._field, this.getHeight(), this.getWidth());
             this._field[holeY][holeX] = hole;
         }
     }
 
     getHatLocation() { // maybe just set it in the beginning
-            let rowX;
-            let rowY;
-            this._field.forEach((row, rowIndex) => {
-                if (row.includes(hat)) {
-                    rowX = row.indexOf(hat);
-                    rowY = rowIndex;
-                }
-            });
-            return [rowX, rowY];
+        let rowX;
+        let rowY;
+        this._field.forEach((row, rowIndex) => {
+            if (row.includes(hat)) {
+                rowX = row.indexOf(hat);
+                rowY = rowIndex;
+            }
+        });
+        return [rowX, rowY];
     }
 
-    
+
     getCurrentLRUDLocInfo(xPlayerLocation, yPlayerLocation) {
         const [leftX, leftY] = [xPlayerLocation - 1, yPlayerLocation];
         const leftChar = this._field[leftY] ? this._field[leftY][leftX] : null;
         const [rightX, rightY] = [xPlayerLocation + 1, yPlayerLocation];
         const rightChar = this._field[rightY] ? this._field[rightY][rightX] : null;
         const [upX, upY] = [xPlayerLocation, yPlayerLocation - 1];
-        const upChar = this._field[upY] ? this._field[upY][upX] : null; 
+        const upChar = this._field[upY] ? this._field[upY][upX] : null;
         const [downX, downY] = [xPlayerLocation, yPlayerLocation + 1];
         const downChar = this._field[downY] ? this._field[downY][downX] : null;
 
@@ -197,10 +238,11 @@ class Field {
             LEFT: [leftChar, leftX, leftY],
             RIGHT: [rightChar, rightX, rightY],
             UP: [upChar, upX, upY],
-            DOWN: [downChar, downX, downY] 
+            DOWN: [downChar, downX, downY]
         }
     }
 
+    // return 1 or 2 dirs dirs either x, y, or x + y,  e.g. ['right'], ['down'], or ['left', 'down'], 
     getDirectionsOfHat() {
         let lrDirection = '';
         let udDirection = '';
@@ -228,75 +270,64 @@ class Field {
         return [RIGHT, LEFT, UP, DOWN];
     }
 
-    storeLastLocation(){
+    storeLastLocation() {
         this._xLastPlayerLocation = this._xPlayerLocation;
         this._yLastPlayerLocation = this._yPlayerLocation;
     }
 
+
+
+    // maze solver utility to ensure user gets a solvable field
     async canSolve(simulate) {
-        // this need to be redone
-        const visitedLocationsInfo = {};
+        const locationInfos = new LocationInfos();
         let simXPlayerLocation = this._xPlayerLocation;
         let simYPlayerLocation = this._yPlayerLocation;
         let canSolve = false;
         let arrived = false;
-        while (!arrived) {         
+        while (!arrived) {
             if (this.hardMode && this.moved && simulate) {
                 this.addRandomNumHoles();
-               }   
-            let prioritizedDirs = this.getDirectionsOfHat().filter((val)=> val);
-            if (!visitedLocationsInfo[simXPlayerLocation + '-' + simYPlayerLocation]){
-                visitedLocationsInfo[simXPlayerLocation + '-' + simYPlayerLocation] = this.getCurrentLRUDLocInfo(simXPlayerLocation, simYPlayerLocation);
             }
-            visitedLocationsInfo[simXPlayerLocation + '-' + simYPlayerLocation] = {...visitedLocationsInfo[simXPlayerLocation + '-' + simYPlayerLocation], ...this.getCurrentLRUDLocInfo(simXPlayerLocation, simYPlayerLocation)};
-            const directionInfo = visitedLocationsInfo[simXPlayerLocation + '-' + simYPlayerLocation];
-            // gather possible directions to try, prioritized by hat direction, not out of bounds, not a hole, 
-            // and not attempted direction from location before
+            let hatDirections = this.getDirectionsOfHat().filter((val) => val);
+            locationInfos.setLocationInfo([simXPlayerLocation, simYPlayerLocation], this.getCurrentLRUDLocInfo(simXPlayerLocation, simYPlayerLocation))
+            const locationInfo = locationInfos.getLocationInfo(simXPlayerLocation, simYPlayerLocation);
             this.LRUD.forEach((direction) => {
-                const [dirChar, dirX, dirY] = directionInfo[direction];
+                const [dirChar] = locationInfo[direction];
                 // ban this direction now and in the future
-                if (!visitedLocationsInfo[simXPlayerLocation + '-' + simYPlayerLocation].bannedDirs) {
-                    visitedLocationsInfo[simXPlayerLocation + '-' + simYPlayerLocation].bannedDirs = [];
-                }
                 if (dirChar === hole || !dirChar) {
-                    if (!visitedLocationsInfo[simXPlayerLocation + '-' + simYPlayerLocation].bannedDirs.includes(direction)){
-                        visitedLocationsInfo[simXPlayerLocation + '-' + simYPlayerLocation].bannedDirs.push(direction);
-                    }
+                    locationInfo.addOneBannedDir(direction)
                 }
-                const bannedDirs = visitedLocationsInfo[simXPlayerLocation + '-' + simYPlayerLocation].bannedDirs || [];
-                if (prioritizedDirs.includes(direction) && bannedDirs.includes(direction)) {
-                    prioritizedDirs.splice(prioritizedDirs.indexOf(direction), 1);
+                if (hatDirections.includes(direction) && locationInfo.hasBannedDir(direction)) {
+                    hatDirections.splice(hatDirections.indexOf(direction), 1);
                 }
-                if (!prioritizedDirs.includes(direction) && !bannedDirs.includes(direction)) {
-                    prioritizedDirs.push(direction);
+                if (!hatDirections.includes(direction) && !locationInfo.hasBannedDir(direction)) {
+                    hatDirections.push(direction);
                 }
             });
             if (this._field[simYPlayerLocation][simXPlayerLocation] === hat) {
                 arrived = true;
                 canSolve = true;
             }
-            if (prioritizedDirs.length) {
+            if (hatDirections.length) {
                 // sort based off of next direction not visited being priority
-                const sortedPrioritizedDirs = prioritizedDirs.sort((a,b) => {
-                    const [, nextXLocA, nextYLocA ] = directionInfo[a];
-                    const [, nextXLocB, nextYLocB ] = directionInfo[b];
-                    if (visitedLocationsInfo[nextXLocA + '-' + nextYLocA] && !visitedLocationsInfo[nextXLocB + '-' + nextYLocB]) {
+                const prioritizedDirs = hatDirections.sort((aDirection, bDirection) => {
+                    const [, nextXLocA, nextYLocA] = locationInfo[aDirection];
+                    const [, nextXLocB, nextYLocB] = locationInfo[bDirection];
+                    if (locationInfos.getLocationInfo(nextXLocA, nextYLocA) && !locationInfos.getLocationInfo(nextXLocB,nextYLocB)) {
                         return 1;
-                    } else if (!visitedLocationsInfo[nextXLocA + '-' + nextYLocA] && visitedLocationsInfo[nextXLocB + '-' + nextYLocB]) {
+                    } else if (locationInfos.getLocationInfo(nextXLocA, nextYLocA) && locationInfos.getLocationInfo(nextXLocB, nextYLocB)) {
                         return -1;
-                    } 
+                    }
                     return 0;
 
                 });
-                const nextDirection = sortedPrioritizedDirs[0];
+                const nextDirection = prioritizedDirs[0];
 
-                if (!visitedLocationsInfo[simXPlayerLocation + '-' + simYPlayerLocation].bannedDirs.includes(nextDirection)){
-                    visitedLocationsInfo[simXPlayerLocation + '-' + simYPlayerLocation].bannedDirs.push(nextDirection);
-                }
-                const [, nextXLoc, nextYLoc ] = directionInfo[nextDirection];
+                locationInfo.addOneBannedDir(nextDirection);
+                const [, nextXLoc, nextYLoc] = locationInfo[nextDirection];
                 simXPlayerLocation = nextXLoc;
                 simYPlayerLocation = nextYLoc;
-                // clean this up by making this more reusable
+                // TODO: clean this up by making this more reusable
                 if (simulate) {
                     this.markFieldLocation();
                     this.print();
@@ -309,17 +340,15 @@ class Field {
                     await setTimeout(500);
                 }
             } else {
-                if (simulate){
+                if (simulate) {
                     this.endGame('No possible solution!')
                 }
                 break;
             }
         }
 
-        // return 2 or 1 dirs dirs either x + y, x, or y, e.g., ['left', 'down'], ['right'], ['down']
-        
         return canSolve;
-       
+
     }
 
     get moved() {
@@ -346,45 +375,47 @@ class Field {
     }
 
     termPrompt() {
-        const prom = new Promise((resolve, reject)=>{
+        const prom = new Promise((resolve) => {
             term.grabInput();
-            term.on( 'key' , function( name , matches , data ) {
+            term.on('key', (name) => {
                 resolve(name);
-                if ( name === 'CTRL_C' ) { Field.terminate(); }
-            } );
+                if (name === 'CTRL_C') { Field.terminate(); }
+            });
         });
         return prom;
     }
 
-    async play(){
+    async play() {
         while (!await this.canSolve()) {
             this.reGenerateField();
         }
         if (this._simulate) {
             await this.canSolve(true);
         } else {
-            while (!this.isGameOver){
-               this.markFieldLocation();
-               if (this.hardMode && this.moved) {
-                this.addRandomNumHoles();
-               }
-               this.print();
-               term('\nUse the arrow keys to move the cursor.')
-               const direction = await this.termPrompt();
-               this.storeLastLocation();
-               this.updateLocation(direction);
-               this.checkOutOfBounds();
-               this.checkInHole();
-               this.checkAtHat();
+            while (!this.isGameOver) {
+                this.markFieldLocation();
+                if (this.hardMode && this.moved) {
+                    this.addRandomNumHoles();
+                }
+                this.print();
+                term('\nUse the arrow keys to move the cursor.')
+                const direction = await this.termPrompt();
+                this.storeLastLocation();
+                this.updateLocation(direction);
+                this.checkOutOfBounds();
+                this.checkInHole();
+                this.checkAtHat();
             }
         }
-        console.log(this.gameOverText);
+        term(this.gameOverText);
         Field.terminate();
     }
 
-    markFieldLocation(){
+    markFieldLocation() {
         this._field[this._yLastPlayerLocation][this._xLastPlayerLocation] = pathCharacter;
         this._field[this._yPlayerLocation][this._xPlayerLocation] = activePathCharacter;
     }
 }
-new Field(Field.generateField(10, 20, 20),{ randomStart: true, hardMode: true, simulate: false}).play();
+
+
+new Field(Field.generateField(10, 20, 20), { randomStart: true, hardMode: true, simulate: true }).play();
